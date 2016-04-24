ee054507a8077efc4b894fdbfb84e9634663fc36
ambitiousank <gankur14@gmail.com>
2016-04-24 20:41:16 +0530

    ok

--- staffmodule/models.py
+++ staffmodule/models.py
@@ -65,8 +65,8 @@ class AdmissionDetail(models.Model):
     admission_id=models.AutoField(primary_key=True)
     roll_number=models.CharField(max_length=32,unique=True)
     status_of_request=models.IntegerField(choices=STATUS_CHOICES, default=1)
-    validated_by=models.CharField(max_length=32,default='ok')
-    validation_comments=models.CharField(max_length=400,blank=True,default='NA')
+    validated_by=models.CharField(max_length=32,default='ok',null=True)
+    validation_comments=models.CharField(max_length=400,blank=True,default='NA',null=True)
     created_on=models.DateField(auto_now_add=True)
 
 
