67c71facf68bb04c57e8e461ad08b623671dca58
Anushree Joshi <anushree3110@gmail.com>
2016-04-29 20:35:53 +0530

    login changes

--- module1/views.py
+++ module1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import SignUp
+from .models import SignUp, MatchRef
 from .forms import SignUpForm, login
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
@@ -36,6 +36,7 @@ def login1(request):
 	if form.is_valid():
 		instance=form.save(commit=False)
 		entry=SignUp.objects.filter(email=instance.email,password=instance.password,role=instance.role,approve='1')
+		#print rnm[0]
 		if not entry:
 			context={        #template context variable and value
 				"form":form,
@@ -44,15 +45,23 @@ def login1(request):
 			return render(request,"login_page.html",context)
 		else:	
 			request.session['role']=entry.values_list('role')[0][0]	
+			request.session['name']=entry.values_list('name')[0][0]	
 			name= entry.values_list('name')[0][0]		
 			context ={
 				"role":instance.role,
 				"title":name,        #template context variable and value
 				}
+			#extracting roll number
 			if instance.role=='Staff':
-				return render(request,"staff_dashboard.html",context)
+				return HttpResponseRedirect("/staff/")
 			if instance.role=='User':
-				return render(request,"success.html",context)
+				entry1=MatchRef.objects.filter(email=instance.email)
+				rnm=entry1.values_list('roll_no')[0][0]
+				print str(rnm)[0]
+				url_student="/candidate_home/"+rnm+"/"
+				print url_student
+				return HttpResponseRedirect(url_student)
 			if instance.role=='Admin':
-				return render(request,"admin_page.html",context)
+				return HttpResponseRedirect("/admin_module/")
 	return render(request,"login_page.html",context)
+
